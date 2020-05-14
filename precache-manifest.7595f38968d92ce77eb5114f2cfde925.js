c9d8fe47bab14832fd6b198f4bef958711458581
daonghia2404 <daonghia2404@gmail.com>
2020-05-14 15:28:41 +0700

    Updates

---
+++ precache-manifest.7595f38968d92ce77eb5114f2cfde925.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d3a067d33f097a98c26fc7bc0ac5df",
+    "url": "/daonghia2404/to-do-lists-app/index.html"
+  },
+  {
+    "revision": "34e662b1ad6019c72333",
+    "url": "/daonghia2404/to-do-lists-app/static/css/2.aa9cacd8.chunk.css"
+  },
+  {
+    "revision": "6d1d2e4680aae1b7bd0d",
+    "url": "/daonghia2404/to-do-lists-app/static/css/main.6a283a1b.chunk.css"
+  },
+  {
+    "revision": "34e662b1ad6019c72333",
+    "url": "/daonghia2404/to-do-lists-app/static/js/2.eacbb8b7.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/daonghia2404/to-do-lists-app/static/js/2.eacbb8b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1d2e4680aae1b7bd0d",
+    "url": "/daonghia2404/to-do-lists-app/static/js/main.88cc93af.chunk.js"
+  },
+  {
+    "revision": "597787a592aec3b98ddd",
+    "url": "/daonghia2404/to-do-lists-app/static/js/runtime-main.00ff1078.js"
+  },
+  {
+    "revision": "9bd8a96e24bc32fa5fd0722de9807e3b",
+    "url": "/daonghia2404/to-do-lists-app/static/media/add.9bd8a96e.svg"
+  },
+  {
+    "revision": "28754f7aad5b676f85716d876d2d8e68",
+    "url": "/daonghia2404/to-do-lists-app/static/media/check-checked.28754f7a.svg"
+  },
+  {
+    "revision": "431f6650178c11296554c3d065be716b",
+    "url": "/daonghia2404/to-do-lists-app/static/media/check.431f6650.svg"
+  },
+  {
+    "revision": "6da0dc5a11cbb40b9546123f2629227a",
+    "url": "/daonghia2404/to-do-lists-app/static/media/trash.6da0dc5a.svg"
+  }
+]);
